c6be5aca6c5399402f69d7a0b395d6bd7168d9c5
PandaDrive <claudiuclau163@gmail.com>
2025-02-16 00:15:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", checkScroll);
   checkScroll();
 });
+document.addEventListener("DOMContentLoaded", function () {
+  let navbar = document.querySelector(".navbar");
+  let lastScrollTop = 0;
+
+  window.addEventListener("scroll", function () {
+    let scrollTop = window.scrollY;
+    if (scrollTop > lastScrollTop) {
+      navbar.style.top = "0"; // Afișează meniul la scroll în jos
+    } else {
+      navbar.style.top = "-60px"; // Ascunde meniul când utilizatorul dă scroll în sus
+    }
+    lastScrollTop = scrollTop;
+  });
+});
